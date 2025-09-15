3c263971c67b906d40004d45f778c98adda8c10e
danieljofficial <joenah004@gmail.com>
2025-09-15 17:12:34 +0100

    fix(authentication):renamed AuthController to authController for consistency

--- apps/api/src/presentation/routes/authRoutes.ts
+++ apps/api/src/presentation/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import "dotenv/config";
 import { Router } from "express";
 import { AuthService } from "../../core/services/authService";
-import { AuthController } from "../controllers/AuthController";
 import { AuthValidators } from "../validators/authValidators";
 import { validateRequest } from "../middlewares/validateRequest";
+import { AuthController } from "../controllers/authController";
+
 const authRoutes = Router();
 
 const authService = new AuthService(process.env.JWT_SECRET as string, 10);
